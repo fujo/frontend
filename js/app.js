49e6ad0f1dd8e11216cca5cb2e132369940b9c96
fujo <fujo@github.com>
2015-07-09 23:31:12 +0200

    stuff

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
-console.log("salut app");
+/*
+Jonathan Fuchs
 
-console.log(Modernizr.touch);
+https://learn.jquery.com/code-organization/concepts/
+
+*/
+(function( $ ){
+
+	var 	touch 	= 	Modernizr.touch;
+
+	var 	win 	= 	$(window),
+			doc		=	$(document);
+
+	var APP = {
+
+		_: function () {
+			console.log(doc.height())
+			this.test._();
+			this.sectionHero._();
+		},
+		// petit
+		// test
+		test : {
+			_: function(){
+				console.log(this.objet);
+			},
+			objet : { "foo":"bar", 99:100}
+		},
+		sectionHero : {
+
+			_: function(){
+				win.on("load resize", function(e){
+					$("#section--1").height( win.height() );
+				});
+			}
+
+		}
+
+	}
+
+
+	APP._();
+})( jQuery );
