4254949fac2629efc5f292a4a843fa17604e29f3
fujo <fujo@github.com>
2015-07-12 23:13:05 +0200

    dev

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ https://learn.jquery.com/code-organization/concepts/
 (function( $ ){
 
 	var 	APP 	= APP || {},
-	 		touch 	= 	Modernizr.touch,
+	 		isTouch = 	Modernizr.touch,
 	 		win 	= 	$(window),
 			doc		=	$(document),
 			APP = {
@@ -15,7 +15,9 @@ https://learn.jquery.com/code-organization/concepts/
 		_: function () {
 			console.log(doc.height())
 			this.test._();
+			this.mainNav._();
 			this.sectionHero._();
+			this.parallax._();
 		},
 		// petit
 		// test
@@ -25,15 +27,39 @@ https://learn.jquery.com/code-organization/concepts/
 			},
 			objet : { "foo":"bar", 99:100}
 		},
-		sectionHero : {
+		mainNav : {
+			_: function(){
 
+				this.mainNav = ('#mainNav');
+
+			},
+			_open: function(){
+
+			},
+			_close: function(){
+
+			}
+		},
+		sectionHero : {
 			_: function(){
 				win.on("load resize", function(e){
 					$("#section--1").height( win.height() );
 				});
 			}
-
-		}
+		},
+		// parallax
+		parallax : {  
+			_: function(){
+				win.on("scroll", function(e){
+				    var scrolled = win.scrollTop();
+    				$('.parallax').css('background-position', 'center ' + (scrolled * -0.2) + 'px');
+    				if(win.scrollTop != 0) 
+    					$(".bounce").css("animation","paused");
+    				else
+    					$(".bounce").css("animation","bounce 2s infinite");
+				});
+			}
+		}      
 
 	};
 
